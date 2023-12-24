16faa66dd9925199ab950a0d0e9f6b8c0316c407
Stefan <105825004+sstoj1@users.noreply.github.com>
2023-12-23 23:28:46 -0500

    Add command line arguments

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-console.log("hello");
-// npm install in order to install all the dependencies to get copy of node modules folder
+function main() {
+  // first input argument is the interpreter, second is a link to main.js, and the third is the user input
+  if (process.argv.length < 3) {
+    console.log("No website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("Only one command line argument allowed");
+    process.exit(1);
+  }
+
+  const URL = process.argv[2];
+  console.log("Crawling " + URL + "...");
+}
+
+main();
